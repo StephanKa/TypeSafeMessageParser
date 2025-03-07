1106fb1fcdd84cd9d8191f751828b772fd764768
Stephan Kantelberg <stephan.kantelberg@zuehlke.com>
2025-03-07 20:16:33 +0100

    fixed conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,3 @@
-import conans.model.requires
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain
 
@@ -8,12 +7,13 @@ class HelloConan(ConanFile):
     generators = 'CMakeDeps', 'CMakeToolchain'
     default_options = {'spdlog/*:header_only': True}
 
+    def requirements(self):
+        self.requires('catch2/3.7.0')
+        self.requires('spdlog/1.14.1')
+
     def configure(self):
         cmake = CMakeToolchain(self)
         cmake.user_presets_path = None
-
-        requirement = ['catch2/3.7.0', 'spdlog/1.14.1']
-        self.requires = conans.model.requires.Requirements(requirement)
 
     def build(self):
         cmake = CMakeToolchain(self)
