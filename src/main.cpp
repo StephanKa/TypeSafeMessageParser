cbb7412356871fa1b666706c55cfbf46fef4f988
Stephan Kantelberg <stephan.kantelberg@zuehlke.com>
2026-05-31 21:00:56 +0200

    added more examples

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <MessageParser.h>
 #include <array>
 #include <cstdint>
+#include <spdlog/spdlog.h>
 
-// example
+// example enums for message fields
 enum class Status : std::uint8_t
 {
     OK,
@@ -34,7 +35,7 @@ enum class Timer : std::uint8_t
     EXIT
 };
 
-// example message definition
+// example message definition using BigEndian (default) and LittleEndian fields
 struct DataFields
 {
     static constexpr auto A = FieldConfiguration<0, Status>{};
@@ -48,6 +49,12 @@ struct DataFields
     static constexpr auto I = FieldConfiguration<8, uint16_t, FieldRanges::MinMaxRange{.min=1000, .max=65000}>{};
 };
 
+// example with little-endian field
+struct LittleEndianFields
+{
+    static constexpr auto sensor = FieldConfiguration<0, uint16_t, FieldRanges::MinMaxRange{.min=uint16_t{0}, .max=uint16_t{4095}}, ByteOrder::LittleEndian>{};
+};
+
 // Message type
 struct Message
 {
@@ -59,21 +66,40 @@ int main()
 {
     constexpr Message msg{.id=42, .data={0x1, 0x3, 0x1, 0x0, 0x4, 0x5, 0xA, 0x5, 0xCF, 0xFF}};
     MessageParser::parseMessage(msg.data, DataFields::A, DataFields::B, DataFields::C, DataFields::D, DataFields::E, DataFields::F, DataFields::G, DataFields::H, DataFields::I);
+
+    // Validate entire message at compile time
+    constexpr bool allValid = MessageParser::validateMessage(msg.data, DataFields::A, DataFields::B, DataFields::C, DataFields::D, DataFields::E, DataFields::F, DataFields::G, DataFields::H, DataFields::I);
+    spdlog::info("All fields valid: {}", allValid);
+
     constexpr auto type = MessageParser::convertByteType(msg.data, DataFields::B);
     if constexpr (type.has_value())
     {
-        spdlog::info("type: {}", static_cast<uint32_t>(type.value()));
+        spdlog::info("type: {}", std::to_underlying(type.value()));
     }
 
     constexpr auto typeH = MessageParser::convertByteType(msg.data, DataFields::H);
     if constexpr (typeH.has_value())
     {
-        spdlog::info("typeH: {}", static_cast<uint32_t>(typeH.value()));
+        spdlog::info("typeH: {}", std::to_underlying(typeH.value()));
     }
+    else
+    {
+        spdlog::error("typeH error: {}", FieldRanges::to_string(typeH.error()));
+    }
+
     constexpr auto typeI = MessageParser::convertByteType(msg.data, DataFields::I);
     if constexpr (typeI.has_value())
     {
-        spdlog::info("typeI: {}", static_cast<uint32_t>(typeI.value()));
+        spdlog::info("typeI: {}", typeI.value());
     }
+
+    // Little-endian example
+    constexpr std::array<uint8_t, 2> sensorMsg = { 0x34, 0x12 }; // 0x1234 in little-endian
+    constexpr auto sensorVal = MessageParser::convertByteType(sensorMsg, LittleEndianFields::sensor);
+    if constexpr (sensorVal.has_value())
+    {
+        spdlog::info("Sensor (LE): {}", sensorVal.value());
+    }
+
     return 0;
 }
